339d20dda178adde9371e4703bbfa64544f20ea5
Markito-TwT <mmn976482@gmail.com>
2025-04-18 23:35:43 -0300

    Add files via upload
    sla so uma base customisavel

---
+++ java.js
@@ -0,0 +1,15 @@
+function eventoClick(){
+alert("Troca de cor");                                      
+document.body.style.backgroundColor = "gray";
+document.text.style.color= "black";
+    // Seleciona o elemento pelo ID
+    var elemento = document.getElementById('texto');
+    
+    // Altera 
+    elemento.style.color = 'black';
+}
+function eventoClick1(){
+    alert("Troca de cor");
+    document.body.style.backgroundColor = "black";
+    document.text.style.color = "black";
+    }
